c469d1ba9e768fd960bd6986143b522f7aad645f
Esteban-224 <besteban224@gmail.com>
2023-04-30 16:56:35 -0600

    Creacion de clase persona

---
+++ src/Persona.java
@@ -0,0 +1,37 @@
+
+public class Persona {
+	
+	
+	String nombre;
+	String correo;
+	String pedido;
+
+
+
+
+		public Persona(String nombre, String correo, String pedido) {
+		super();
+		this.nombre = nombre;
+		this.correo = correo;
+		this.pedido = pedido;
+		}
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
