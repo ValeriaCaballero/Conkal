3ebf7b4c2a51c1c5d365e09f5250e260676c6511
Valeria <A18003354@alumnos.uady.mx>
2023-05-05 18:22:12 -0500

    Agregué un combo y el botón de limpiar

--- src/GUI.java
+++ src/GUI.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -18,6 +19,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.awt.Color;
+import javax.swing.JComboBox;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 @SuppressWarnings("unused")
 public class GUI extends JFrame {
@@ -25,8 +29,22 @@ public class GUI extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField_nombre;
 	private JTextField textField_correo;
-	private JTextField textField_pedido;
 	
+	Medicinas seleccionado; 
+	
+    DefaultComboBoxModel combomedicinas = new DefaultComboBoxModel();
+	
+	public Medicinas arreglomedicinas[]= {
+			new Medicinas ("Elige tu pedido ",0),
+			new Medicinas ("Paracetamol", 30),		
+			new Medicinas ("Ibuprofeno", 45),
+			new Medicinas ("Aspirina", 40),
+			new Medicinas ("Ramipril", 55),
+			new Medicinas ("Omeprazol", 120),
+			new Medicinas ("Salbutamol", 60),
+			new Medicinas ("Loratadina", 30),
+			new Medicinas ("Levotiroxina", 60),
+	}; 
 
 	/**
 	 * Launch the application.
@@ -49,7 +67,7 @@ public class GUI extends JFrame {
 	 */
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 550, 384);
+		setBounds(100, 100, 550, 414);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(205, 254, 249));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,43 +82,84 @@ public class GUI extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setBounds(100, 81, 61, 14);
+		lblNombre.setBounds(100, 65, 61, 14);
 		lblNombre.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
 		contentPane.add(lblNombre);
 		
 		JLabel lblCorreo = new JLabel("Correo ");
-		lblCorreo.setBounds(100, 141, 61, 14);
+		lblCorreo.setBounds(100, 125, 61, 14);
 		lblCorreo.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
 		contentPane.add(lblCorreo);
 		
 		JLabel lblPedido = new JLabel("Pedido");
-		lblPedido.setBounds(100, 204, 61, 14);
+		lblPedido.setBounds(100, 188, 61, 14);
 		lblPedido.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
 		contentPane.add(lblPedido);
 		
 		textField_nombre = new JTextField();
 		textField_nombre.setForeground(new Color(0, 0, 0));
 		textField_nombre.setBackground(new Color(223, 223, 223));
-		textField_nombre.setBounds(100, 110, 313, 20);
+		textField_nombre.setBounds(100, 94, 313, 20);
 		textField_nombre.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
 		contentPane.add(textField_nombre);
 		textField_nombre.setColumns(10);
 		
 		textField_correo = new JTextField();
 		textField_correo.setBackground(new Color(223, 223, 223));
-		textField_correo.setBounds(100, 166, 313, 20);
+		textField_correo.setBounds(100, 150, 313, 20);
 		textField_correo.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
 		textField_correo.setColumns(10);
 		contentPane.add(textField_correo);
 		
-		textField_pedido = new JTextField();
-		textField_pedido.setBackground(new Color(223, 223, 223));
-		textField_pedido.setBounds(100, 227, 313, 20);
-		textField_pedido.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
-		textField_pedido.setColumns(10);
-		contentPane.add(textField_pedido);
+		JLabel lblPrecioPorPieza = new JLabel("Precio por pieza");
+		lblPrecioPorPieza.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
+		lblPrecioPorPieza.setBounds(100, 213, 110, 14);
+		contentPane.add(lblPrecioPorPieza);
 		
-		JButton btnESCRIBIR = new JButton("Escribir");
+		JLabel lblprecio = new JLabel("...");
+		lblprecio.setHorizontalAlignment(SwingConstants.CENTER);
+		lblprecio.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
+		lblprecio.setBounds(255, 213, 61, 14);
+		contentPane.add(lblprecio);
+		
+		JButton btnComprar = new JButton("Elegir");
+		btnComprar.setFont(new Font("Tw Cen MT", Font.BOLD, 15));
+		btnComprar.setBackground(new Color(128, 128, 255));
+		btnComprar.setBounds(354, 232, 77, 23);
+		contentPane.add(btnComprar);
+		
+		JLabel lblPrecioTotal = new JLabel("Precio Total");
+		lblPrecioTotal.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
+		lblPrecioTotal.setBounds(100, 265, 110, 14);
+		contentPane.add(lblPrecioTotal);
+		
+		JLabel lblprecio_total = new JLabel("...");
+		lblprecio_total.setHorizontalAlignment(SwingConstants.CENTER);
+		lblprecio_total.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
+		lblprecio_total.setBounds(255, 266, 61, 14);
+		contentPane.add(lblprecio_total);
+		
+		JLabel lblColocaElNmero = new JLabel("Coloca el número de piezas a comprar");
+		lblColocaElNmero.setFont(new Font("Tw Cen MT", Font.PLAIN, 15));
+		lblColocaElNmero.setBounds(100, 238, 248, 14);
+		contentPane.add(lblColocaElNmero);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				seleccionado=(Medicinas)comboBox.getSelectedItem();
+				
+				lblprecio.setText(String.valueOf(seleccionado.getPrecio()));
+			}
+		});
+		comboBox.setBounds(157, 185, 191, 17);
+		contentPane.add(comboBox);
+		
+		for(int i=0; i<arreglomedicinas.length; i++)
+			combomedicinas.addElement(arreglomedicinas[i]);
+		comboBox.setModel(combomedicinas);
+		
+		JButton btnESCRIBIR = new JButton("Agregar Pedido");
 		btnESCRIBIR.setBackground(new Color(128, 128, 255));
 		btnESCRIBIR.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -109,7 +168,7 @@ public class GUI extends JFrame {
 					
 					String textNom = textField_nombre.getText();
 					String textCor = textField_correo.getText();
-					String textPedido = textField_pedido.getText();
+					String textPedido =comboBox.getSelectedItem().toString();
 					String cadena = ("Cliente:" + " " + textNom + " " + "  " + "Correo electrónico:" + "  " + textCor + " " + "Pedido:" + " " + textPedido );
 				
 				
@@ -124,7 +183,7 @@ public class GUI extends JFrame {
 			}	
 		});
 		btnESCRIBIR.setFont(new Font("Tw Cen MT", Font.BOLD, 15));
-		btnESCRIBIR.setBounds(55, 273, 89, 23);
+		btnESCRIBIR.setBounds(64, 307, 132, 23);
 		contentPane.add(btnESCRIBIR);
 		
 		JButton btnLeer = new JButton("Leer ");
@@ -151,20 +210,22 @@ public class GUI extends JFrame {
 		
 		});
 		btnLeer.setFont(new Font("Tw Cen MT", Font.BOLD, 15));
-		btnLeer.setBounds(227, 274, 89, 23);
+		btnLeer.setBounds(237, 307, 89, 23);
 		contentPane.add(btnLeer);
 		
-		JButton btnLimpiar = new JButton("Limpiar");
+		JButton btnLimpiar = new JButton("Ingresar nuevo");
 		btnLimpiar.setBackground(new Color(128, 128, 255));
 		btnLimpiar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField_nombre.setText("");
 				textField_correo.setText("");
-				textField_pedido.setText("");
+				
+				lblprecio.setText(null); 
+				lblprecio_total.setText(null); 
 			}
 		});
 		btnLimpiar.setFont(new Font("Tw Cen MT", Font.BOLD, 15));
-		btnLimpiar.setBounds(55, 311, 89, 23);
+		btnLimpiar.setBounds(214, 341, 132, 23);
 		contentPane.add(btnLimpiar);
 		
 		JButton btnObjeto = new JButton("Objeto");
@@ -174,16 +235,17 @@ public class GUI extends JFrame {
 				
 				String textNom = textField_nombre.getText();
 				String textCor = textField_correo.getText();
-				String textPedido = textField_pedido.getText();
+				String textPedido =comboBox.getSelectedItem().toString();
 				
 				Persona objpersona = new Persona(textNom,textCor,textPedido); 
 				JOptionPane.showMessageDialog(btnObjeto, "Mi objeto tiene lo siguiente:"+ ""+ objpersona, null, JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 		btnObjeto.setFont(new Font("Tw Cen MT", Font.BOLD, 15));
-		btnObjeto.setBounds(378, 273, 89, 23);
+		btnObjeto.setBounds(378, 307, 89, 23);
 		contentPane.add(btnObjeto);
+		
+		
 		}
-	
 	}//ULTIMO
 
